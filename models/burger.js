b885c42653c2092de85e9ffa95c12103d67eb6fd
ajschilt <ajschilt86@gmail.com>
2018-09-11 12:42:20 -0500

    working

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,23 @@
 const orm = require("../config/orm.js");
 
 var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
             cb(res);
         });
     },
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
             cb(res);
         });
     },
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, function (res) {
             cb(res);
         });
     }
